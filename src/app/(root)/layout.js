6bfdca871acd41344bcf1282a09c651b9a0a8e9f
SadAfrin <113997966+SadAfrin@users.noreply.github.com>
2026-06-18 23:14:23 +0600

    better-auth, mongodb, google providor setup and some other set up done

---
+++ src/app/(root)/layout.js
@@ -0,0 +1,14 @@
+import Navbar from "@/components/shared/Navbar";
+import Footer from "@/components/shared/Footer";
+
+export default function RootLayout({ children }) {
+  return (
+    <div>
+      <Navbar />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
---
+++ src/lib/auth.js
@@ -0,0 +1,31 @@
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
+
+const client = new MongoClient(process.env.MONGODB_URI);
+await client.connect();
+const db = client.db();
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, { client }),
+
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    },
+  },
+
+  user: {
+    additionalFields: {
+      role: {
+        type: "string",
+        defaultValue: "buyer",
+      },
+    },
+  },
+});
